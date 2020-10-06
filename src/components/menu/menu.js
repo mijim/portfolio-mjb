469e9e6bd69dea2dfda1f1f186de6938124365aa
Miguel <migueljimenezbenajes@gmail.com>
2020-10-06 16:42:10 +0200

    change style

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -4,34 +4,42 @@ import "./menu.css";
 const Menu = props => {
   const { hoveredSection, onClickSection, currentSection } = props;
   return (
-    <div className={`menu-container ${currentSection === "" ? "home" : ""}`}>
-      <div
-        className={`menu-section ${
-          hoveredSection === "projects" ? "hovered" : ""
-        }
+    <>
+      <div className={`menu-container ${currentSection === "" ? "home" : ""}`}>
+        {currentSection === "" && (
+          <div className="mjb-title">Miguel Jiménez Benajes</div>
+        )}
+
+        <div
+          className={`menu-section ${
+            hoveredSection === "projects" ? "hovered" : ""
+          }
         ${currentSection === "projects" ? "clicked" : ""}
         `}
-        onClick={() => onClickSection("projects")}
-      >
-        Proyectos
-      </div>
-      <div
-        className={`menu-section ${
-          hoveredSection === "contact" ? "hovered" : ""
-        }
+          onClick={() => onClickSection("projects")}
+        >
+          Proyectos
+        </div>
+        <div
+          className={`menu-section ${
+            hoveredSection === "contact" ? "hovered" : ""
+          }
         ${currentSection === "contact" ? "clicked" : ""}`}
-        onClick={() => onClickSection("contact")}
-      >
-        Contacto
-      </div>
-      <div
-        className={`menu-section ${hoveredSection === "about" ? "hovered" : ""}
+          onClick={() => onClickSection("contact")}
+        >
+          Contacto
+        </div>
+        <div
+          className={`menu-section ${
+            hoveredSection === "about" ? "hovered" : ""
+          }
         ${currentSection === "about" ? "clicked" : ""}`}
-        onClick={() => onClickSection("about")}
-      >
-        Sobre mí
+          onClick={() => onClickSection("about")}
+        >
+          Sobre mí
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
