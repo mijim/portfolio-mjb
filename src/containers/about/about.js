783f993cae99f055cd1789154bab88068855e256
Miguel <migueljimenezbenajes@gmail.com>
2020-10-22 12:58:58 +0200

    fix: about page

--- src/containers/about/about.js
+++ src/containers/about/about.js
@@ -32,14 +32,26 @@ const About = props => {
       </div>
       <div className="about-text">
         <div className="about-title">{`Software Engineer \n& Front-End DEV`}</div>
-        Ingeniero Informático con especialidad en el desarrollo de software. Me
-        he dedicado al desarrollo desde que descubrí el mundo de la programación
-        y he decantado toda mi vida profesional en esta dirección. Pasando por
-        trabajo de investigación en la universidad hasta un par de compañías de
-        software de Madrid, adquiriendo así amplios conocimientos en el área de
-        Front-End y desarrollo de aplicaciones tanto Web como Móvil. Con un gran
-        interés en generar experiencias inmersivas en 3D, mis últimos esfuerzos
-        se han volcado en aprender a crear entornos virtuales con WebGL que
+        <div className="colored-b">
+          Ingeniero Informático con especialidad en el desarrollo de software.
+        </div>{" "}
+        Me cautivó la programación desde el principio y he decantado toda mi
+        vida profesional en esta dirección.
+        <br />
+        <br />
+        Pasando por trabajo de investigación en la universidad hasta un par de
+        compañías de software de Madrid, he tenido tenido la oportunidad de
+        trabajar en proyectos con grandes empresas como{" "}
+        <div className="colored-b">Asisa, Samsung o HBO</div> entre otras,
+        adquiriendo así amplios conocimientos en el área de{" "}
+        <div className="colored-b">
+          Front-End y desarrollo de aplicaciones tanto Web como Móvil.
+        </div>
+        <br />
+        <br />
+        Con un gran interés en generar experiencias inmersivas en 3D, mis
+        últimos esfuerzos se han volcado en aprender a crear{" "}
+        <div className="colored-b">entornos virtuales con WebGL</div> que
         aporten un gran valor al UX.
       </div>
     </div>
